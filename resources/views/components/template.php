3d86ad3e5a4664edb73db4cc4bbcb8722f4bf8e5
Angel Garcia <git@angelgarcia.dev>
2024-05-10 02:03:35 -0600

    [Adv] Primer avance significativo al front-end.

--- resources/views/components/template.php
+++ resources/views/components/template.php
@@ -1,58 +1,28 @@
-  <!DOCTYPE html>
-  <html>
+<!DOCTYPE html>
+<html>
 
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-    <title><?= $title ?? 'Furrys de Juárez' ?></title>
+  <title><?= $title ?? 'Furrys de Juárez' ?></title>
 
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" />
-    <link rel="stylesheet" href="<?= wwwPath('css/styles.css', true) ?>">
-  </head>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" />
+  <link rel="stylesheet" href="<?= wwwPath('css/styles.css', true) ?>">
 
-  <body>
-    <nav>
-      <div class="nav-wrapper">
-        <a href="/" class="brand-logo"><img src="<?= wwwPath('img/furrysdejuarez.webp') ?>" alt="Furrys de Juárez"></a>
-        <ul id="nav-mobile" class="right hide-on-med-and-down">
-          <li><a href="sass.html">Sass</a></li>
-          <li><a href="badges.html">Components</a></li>
-          <li><a href="collapsible.html">JavaScript</a></li>
-        </ul>
-      </div>
-    </nav>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+  <script src="<?= wwwPath('js/scripts.js') ?>"></script>
+</head>
 
-    <main><?= ($body ?? function () {})() ?></main>
+<body>
+  <?php view('components/nav'); ?>
 
-    <footer class="page-footer">
-      <div class="container">
-        <div class="row">
-          <div class="col l6 s12">
-            <h5 class="white-text">Footer Content</h5>
-            <p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
-          </div>
-          <div class="col l4 offset-l2 s12">
-            <h5 class="white-text">Links</h5>
-            <ul>
-              <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
-              <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
-              <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
-              <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
-            </ul>
-          </div>
-        </div>
-      </div>
-      <div class="footer-copyright">
-        <div class="container">
-          © 2014 Copyright Text
-          <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
-        </div>
-      </div>
-    </footer>
+  <main><?php is_callable($body) && $body() ?></main>
 
-    <script type=" text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-  </body>
+  <?php view('components/footer'); ?>
 
-  </html>
+  <script type=" text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+</body>
+
+</html>
--- resources/views/index.php
+++ resources/views/index.php
@@ -1,16 +1,54 @@
 <?php
 
-$body = function () {
-    ?>
-    <div class="container">
-      <h1>Furrys de Juárez!</h1>
-      <p>Aún estoy construyendo esta madre.</p>
+$body = function () { ?>
+  <div class="container row">
+    <div class="col s12 m6 l4 right card card-hero">
+      <div class="card-image">
+        <img src="<?= wwwPath('img/furrysdejuarez-hero.webp') ?>" alt="">
+      </div>
+      <div class="card-content">
+        <p>
+          Logotipo de la comunidad. <br>
+          Autor: <a href="#">AstaDraws</a>
+        </p>
+      </div>
     </div>
-    <?php
-};
+    <div class="col s12 m6 l8 left">
+      <h1>¿Quienes somos?</h1>
+      <p>
+        Somos un grupo cultural sin fines de lucro que busca promover y difundir
+        la cultura furry en la ciudad de Juárez, México. <br>
+        Nos enfocamos en organizar eventos, talleres y actividades relacionadas
+        con el arte y la comunidad furry.
+      </p>
+      <p>
+        Contrario a la creencia popular, no somos una ONG o asociación civil.
+        Somos simplemente un grupo de amigos que comparten la pasión por la cultura
+        furry. <br>
+        Nos reunimos de manera informal para disfrutar de actividades relacionadas
+        al fandom. Jugamos, compartimos y convivimos, siempre de forma informal,
+        y ocacionalmente nos involucramos en eventos culturales y de difusión.
+      </p>
+    </div>
+  </div>
+  <div class="parallax-container" style="height: 200px;">
+    <div class="parallax">
+      <img src="<?= wwwPath('img/photos/photo_2023-12-28_22-05-03.webp') ?>" alt="Foto Netovens" style="width: 110vw;">
+    </div>
+  </div>
+  <div class="container">
+    <h1>Reuniones</h1>
+    <p>
+      Nuestras reuniones son aleatorias y casuales, a veces poniendonos de
+      acuerdo el mismo día... <b>LOL</b><br>
+      Te invitamos a contactarnos en nuestros
+      <a href="/contacto">canáles oficiales</a> para que puedas si deseas unirte
+      a nuestras reuniones o eventos.
+    </p>
+  </div>
+<?php };
 
 view('components/template', [
   'title' => 'Inicio 🐾 Furrys de Juárez',
   'body'  => $body,
 ]);
-
