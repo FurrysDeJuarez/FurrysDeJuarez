fc7c9e44b4a87aa77786ac802b416c017318fab9
Angel Garcia <git@angelgarcia.dev>
2024-05-10 18:42:40 -0600

    [Adv] Corrección en el archivo de helpers y footer cambiado.

--- resources/views/components/footer.php
+++ resources/views/components/footer.php
@@ -1,25 +1,28 @@
 <footer class="page-footer">
-  <div class="container">
-    <div class="row">
-      <div class="col l6 s12">
-        <h5 class="white-text">Footer Content</h5>
-        <p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
-      </div>
-      <div class="col l4 offset-l2 s12">
-        <h5 class="white-text">Links</h5>
-        <ul>
-          <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
-          <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
-          <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
-          <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
-        </ul>
-      </div>
+  <div class="container row">
+    <div class="col s12 m6 right">
+      Sígenos en: <br>
+      <a target="_blank" href="https://t.me/+ZzBEFVBcd6FhYzNh" class="fa-brands fa-telegram"></a>
+      <a target="_blank" href="https://www.facebook.com/groups/174033883419156" class="fa-brands fa-facebook"></a>
+      <a target="_blank" href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" class="fa-solid fa-paw"></a>
     </div>
   </div>
   <div class="footer-copyright">
     <div class="container">
-      © 2014 Copyright Text
-      <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+      © 2024 <a href="https://www.angelgarcia.dev/" target="_blank">Angel Garcia</a> | Furrys de Juárez. Algunos derechos reservados.
+
+      <a href="//github.com/FurrysDeJuarez/FurrysDeJuarez" class="right"><i class="fab fa-github"></i></a>
     </div>
   </div>
 </footer>
+
+<script>
+  $(function() {
+    $('[href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"]')
+      .attr('href', 'https://onlyfans.com/murrsuitJuarez')
+      .click(function(evt) {
+        evt.preventDefault();
+        window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ', '_blank'); // <- Sí, es un Rickroll
+      })
+  })
+</script>
