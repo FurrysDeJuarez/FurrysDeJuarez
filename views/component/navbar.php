a329aceac2f4770157129f1d8bc7ebd73894a9fd
Angel Garcia <git@angelgarcia.dev>
2026-04-06 15:49:46 -0600

    feat(ui): rediseñar barra de navegación e integrar fuentes personalizadas
    - Se integran las fuentes 'Metrophobic' y 'Cousine' a través de bunny.net.
    - Se actualiza la estructura y el posicionamiento del menú desplegable en el componente navbar.
    - Se añaden nuevos enlaces de navegación: Acerca de, Contacto y FAQ.
    - Se aplican ajustes tipográficos globales y estilos para elementos de código.

--- views/component/navbar.php
+++ views/component/navbar.php
@@ -4,15 +4,20 @@
       <img src="/img/furrysdejuarez.webp" alt="Furrys de Juarez" class="img-fluid">
     </a>
 
-    <input type="checkbox" id="nav-toggle" class="spoiler">
-    <div class="spoiler">
-      <ul class="navbar-nav">
-        <li class="nav-item"><a href="/" title="Inicio">Inicio</a></li>
-      </ul>
-    </div>
-
     <label for="nav-toggle" class="nav-toggle">
       <i class="fas fa-bars"></i>
     </label>
   </div>
 </nav>
+
+<input type="checkbox" id="nav-toggle" class="spoiler">
+<div class="spoiler navbar-menu">
+  <div class="container">
+    <div class="nav-elements">
+      <a href="/" title="Inicio">Inicio</a>
+      <a href="/acerca-de" title="Acerca de">Acerca de</a>
+      <a href="/contacto" title="Contacto">Contacto</a>
+      <a href="/faq" title="Preguntas frecuentes">Preguntas frecuentes</a>
+    </div>
+  </div>
+</div>
