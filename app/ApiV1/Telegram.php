975f0faacd756a1472cd22241639633f1612b1ff
Angel Garcia <git@angelgarcia.dev>
2026-04-15 01:38:47 -0600

    feat(api): implementar integración con Telegram y rutas de API v1
    Se establece la infraestructura para la mensajería y el ruteo de la API:
    - Se crea el controlador App\ApiV1\Telegram para gestionar peticiones al API y webhooks.
    - Se implementa el manejo de comandos (/start, /help, /bank) con persistencia en DB.
    - Se añade un sistema de logs especializado para transacciones de Telegram.
    - Se configuran las rutas para /api/v1/ y se definen prefijos para aplicaciones.

---
+++ app/ApiV1/Telegram.php
@@ -0,0 +1,233 @@
+<?php
+
+namespace App\ApiV1;
+
+use App\DB\Models\TelegramMessage;
+use stdClass;
+
+defined('TELEGRAM_CACHE_PATH') || define('TELEGRAM_CACHE_PATH', CACHE_PATH . '/Telegram');
+
+class Telegram
+{
+  /**
+   * Llama al API de Telegram y lo guarda en el log, y regresa el resultado como
+   * `stdClass` o `null` si se llama de manera asincrona.
+   *
+   * @param string $cMethod
+   * @param array $aData
+   * @param bool $bAsync
+   * @return stdClass|null
+   */
+  public static function Api(string $cMethod, array $aData, bool $bAsync = false): ?stdClass
+  {
+    static $cApikey = Env('TG_APIKEY');
+    if (!$cApikey) throw new \RuntimeException('TG_APIKEY is not set');
+
+    $cPayload = json_encode($aData, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+    static::Log("$cMethod > $cPayload");
+    if (strlen($cPayload) > 150) {
+      // is_dir(TELEGRAM_CACHE_PATH) || mkdir(TELEGRAM_CACHE_PATH, 0775, true);
+      static $dummy = ShellExec("mkdir -p '" . TELEGRAM_CACHE_PATH . "';" .
+        "find '" . TELEGRAM_CACHE_PATH . "' -type f -mmin +5 -delete", true);
+      $uHash = hexdec(hash('crc32', $cPayload));
+      $cPath = sprintf(
+        "%s/%x.json",
+        TELEGRAM_CACHE_PATH,
+        (time() << 8) | $uHash
+      );
+      file_put_contents($cPath, $cPayload);
+      $cPayload = "@$cPath";
+    }
+
+    $cCommand = "curl -d '$cPayload' -H 'Content-Type: application/json' "
+      . "'https://api.telegram.org/bot$cApikey/$cMethod'";
+
+    if ($bAsync) {
+      ShellExec($cCommand, true);
+      static::Log("< (i) Async call, ignore result");
+      return null;
+    }
+
+    $aResult = ShellExec($cCommand);
+    $oResult = json_decode(implode(PHP_EOL, $aResult));
+    $cResult = json_encode($oResult, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES); // Normalizar a una linea
+    static::Log("< $cResult");
+    return $oResult;
+  }
+
+  /**
+   * Procesa los webhooks de Telegram
+   *
+   * @return void
+   */
+  public static function POST()
+  {
+    $cInput = file_get_contents('php://input');
+    $aInput = json_decode($cInput, true);
+    if (!$aInput) {
+      static::Log('(!) No se pudo decodificar el mensaje');
+      static::Log("< $cInput");
+      http_response_code(400);
+      return;
+    }
+
+    $aInput = array_keys($aInput);
+    $oInput = json_decode($cInput);
+    $cInput = json_encode($oInput, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+    static::Log("(webhook) < $cInput");
+    foreach ($aInput as $cKey) switch ($cKey) {
+      case 'message':
+        static::HandleMessage($oInput->message);
+        continue 2;
+      default:
+        continue 2;
+    }
+  }
+
+  // Funciones internas
+  /**
+   * Maneja los mensajes entrantes
+   *
+   * @param object $oMessage
+   * @return void
+   */
+  protected static function HandleMessage(object $oMessage): void
+  {
+    $iChatId    = $oMessage->chat->id;
+    $uMessageId = $oMessage->message_id;
+    $uUser      = $oMessage->from->id;
+    $cUser      = $oMessage->from->username;
+
+    // Registra el mensaje en log y DB.
+    static::Log(sprintf(
+      '(i) UserID: %d, ChatID: %d, Username: %s',
+      $uUser,
+      $iChatId,
+      $cUser
+    ));
+
+    TelegramMessage::Create([
+      'ChatId'    => $iChatId,
+      'MessageId' => $uMessageId,
+      'UserId'    => $uUser,
+      'Username'  => $cUser,
+    ]);
+
+    // Comandos de Telegram
+    switch (strtolower(trim($oMessage->text))) {
+      case '/start':
+      case '/comenzar':
+        // Evento de actividad.
+        static::Api('sendChatAction', [
+          'chat_id' => $iChatId,
+          'action' => 'typing',
+        ], true);
+
+        // Mensaje de bienvenida.
+        $aTextMessage = ['Bienvenido a Furrys de Juarez'];
+        $cAppEnv = Env('APP_ENV');
+        if ($cAppEnv != 'prod') {
+          $mAppVersion = Env('APP_VERSION', 'SNAPSHOT');
+          // $aTextMessage = array_merge($aTextMessage, [
+          //   PHP_EOL,
+          //   '```',
+          //   "AppEnv: $cAppEnv",
+          //   "AppVersion: $mAppVersion",
+          //   '```',
+          // ]);
+          $aTextMessage = [
+            ...$aTextMessage,
+            // '',
+            '```',
+            "AppEnv: $cAppEnv",
+            "AppVersion: $mAppVersion",
+            '```',
+          ];
+        } else $aTextMessage[] = '';
+
+        $aTextMessage = [
+          ...$aTextMessage,
+          // PHP_EOL,
+          // '',
+          'Soy Beanites, mascota de la comunidad de Furrys de Juarez.',
+          '',
+          'Conmigo puedes consultar información relacionada con tu perfil dentro de la comunidad.',
+          '',
+          'Estos son los comandos disponibles:',
+          '/start - Recomienza la sesión. Esto no elimina tu información.',
+          '/help - Muestra los comandos disponibles.',
+          '/bank - Te permite acceder a tu cuenta bancaria. (sistema de puntos)',
+        ];
+
+        $cTextMessage = implode(PHP_EOL, $aTextMessage);
+        $oRes = static::Api('sendMessage', [
+          'chat_id' => $iChatId,
+          'text' => $cTextMessage,
+          'parse_mode' => 'Markdown',
+          'entities' => [
+            ['type' => 'bot_command', ...TextOffset('/start', $cTextMessage)],
+            ['type' => 'bot_command', ...TextOffset('/help',  $cTextMessage)],
+            ['type' => 'bot_command', ...TextOffset('/bank',  $cTextMessage)],
+          ],
+        ]);
+        $oResult = $oRes->result;
+        TelegramMessage::Create([
+          'ChatId'    => $oResult->chat->id,
+          'MessageId' => $oResult->message_id,
+          'UserId'    => $oResult->from->id,
+          'Username'  => $oResult->from->username,
+        ]);
+
+        $aMessages = range(max(1, $oResult->message_id - 99), $oResult->message_id - 1);
+        if (count($aMessages) > 0) {
+          static::Api('deleteMessages', [
+            'chat_id' => $oResult->chat->id,
+            'message_ids' => $aMessages,
+          ], true);
+
+          TelegramMessage::Delete([
+            'ChatId' => $oResult->chat->id,
+            'MessageId' => $aMessages,
+          ]);
+        }
+        break;
+      case '/help':
+      case '/ayuda':
+      case '/bank':
+        // TODO.
+        $oResult = static::Api('sendMessage', [
+          'chat_id' => $iChatId,
+          'text' => 'Aún en desarrollo.',
+        ]);
+        TelegramMessage::Create([
+          'ChatId'    => $oResult->result->chat->id,
+          'MessageId' => $oResult->result->message_id,
+          'UserId'    => $oResult->result->from->id,
+          'Username'  => $oResult->result->from->username,
+        ]);
+        break;
+      default:
+        break;
+    }
+  }
+
+
+  /**
+   * Register a log to Telegram log file
+   *
+   * @param string $cMessage
+   * @return void
+   */
+  public static function Log(string $cMessage): void
+  {
+    static $rLogger = fopen(STORAGE_PATH . '/Logs/Telegram.log', 'a+');
+    static $uLogger = (time() << 4) + random_int(0, 15);
+    if (!$rLogger) return;
+    fwrite($rLogger, sprintf(
+      '[%s] %012x :: %s' . PHP_EOL,
+      date('Y-m-d H:i:s'),
+      $uLogger,
+      $cMessage
+    ));
+  }
+}
--- app/App.php
+++ app/App.php
@@ -10,5 +10,18 @@ class App
   {
     // Index
     Router::get('/', [Routes::class, 'Index']);
+
+    // Apps
+    Router::$CriteriaPrefix = '/Apps';
+    Router::ANY('/Puntos', function () { ## Placeholder, temporal.
+      header('Content-Type: text/plain; charset=utf-8');
+      echo 'Sistema de Puntos';
+    });
+
+    // Api
+    Router::USE('App\\ApiV1', '/api/v1/');
+
+    ## 404
+    http_response_code(404);
   }
 }
